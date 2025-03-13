2eb4094e33567e5e043b949c7df2f54ce70faa40
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 11:10:13 +0700

    akhir

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022330077
+{
+    internal class fanLaptop
+    {
+        enum State { turbo,balanced,exit,start};
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,106 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Security.Cryptography.X509Certificates;
+using modul4_103022330077;
+
+internal class fanLaptop
+{
+    enum State { turbo, balanced, quite, performance };
+
+    State fanState = State.quite;
+    public fanLaptop()
+    {
+         private void turboOff()
+    {
+        if (this.fanState == State.turbo)
+        {
+            fanState = State.quite;
+        }
+    }
+    private void turboOn()
+    {
+        if (this.fanState == State.quite)
+        {
+            fanState = State.turbo;
+        }
+    }
+    private void modeUp()
+    {
+        if (this.fanState == State.quite)
+        {
+            fanState = State.balanced;
+            Console.WriteLine("fan quite beruban menjadi balnced");
+        }
+        else if (this.fanState == State.balanced)
+        {
+            Console.WriteLine("fan quite beruban menjadi performance");
+            fanState = State.performance;
+        }
+        else if (this.fanState == State.performance)
+        {
+            fanState = State.turbo;
+            Console.WriteLine("fan quite beruban menjadi turbo");
+        }
+        else if (this.fanState == State.turbo)
+        {
+            fanState = State.turbo;
+            Console.WriteLine("fan quite mencapai batas");
+
+        }
+    }
+
+    public void modeDown()
+    {
+        if (this.fanState == State.turbo)
+        {
+            fanState = State.performance;
+            Console.WriteLine("fan quite berubah menjadi performance");
+        }
+        else if (this.fanState == State.performance)
+        {
+            fanState = State.balanced;
+            Console.WriteLine("fan quite beruban menjadi balanced");
+        }
+        else if (this.fanState == State.balanced)
+        {
+            fanState = State.quite;
+            Console.WriteLine("fan quite beruban menjadi quite");
+        }
+        else if (this.fanState == State.quite)
+        {
+            fanState = State.quite;
+            Console.WriteLine("fan quite sudah quite");
+        }
+    }
+}
+ 
+
+    
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produk kode = new Produk();
+        Console.WriteLine("Kode Produk Elektronik");
+        for (int i = 0; i < kode.kodeProduk.Length; i++)
+        {
+            Console.WriteLine(kode.kodeProduk[i] + " - " + kode.produkElektronik[i]);
+        }
+        fanLaptop fan = new fanLaptop();
+
+        fan.modeUp();
+        fan.modeUp();
+        fan.modeUp();
+        fan.modeDown();
+        fan.modeDown();
+        fan.modeDown();
+
+    }
+}
+public class Produk
+{
+
+    public string[] kodeProduk = { "E100", "E101", "E102", "E103", "E104", "E105", "E106", "E107", "E108", "E109" };
+    public string[] produkElektronik = { "laptop", "smartphone", "tablet", "hedset", "keyboard", "mouse", "printer", "monitor", "smartwatch", "kamera" };
+
+}
